1b6d2cf90f3289d602af67c7f3fc249a2943410c
zzachchase145 <zzachchase@gmail.com>
2026-05-21 04:40:21 +0100

    trying to make main route

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('git is working!!!')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# getting a basic get route to get my server running
+
+@app.get('/')
+async def root():
+    print('server is working!')
+
+
+@app.post('/customer_complaints')
+
+def recieve_customer_complaint():
+    
