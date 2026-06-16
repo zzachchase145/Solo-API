221b6b69b50563a696b0e7eccae60146e7fc6608
zzachchase145 <zzachchase@gmail.com>
2026-06-16 05:05:40 +0100

    few touch ups

--- models/hotel_complaints.py
+++ models/hotel_complaints.py
@@ -9,8 +9,15 @@ from datetime import datetime
 # CUSTOMER COMPLAINT HOTEL POST PYDANTIC MODELS
 # ===================================================================================================
 
+# This is where we will validate all our fields for the moment we will allow them to all accept None for testing purposes.
+
 class CreateHotelCustomerComplaint(BaseModel):
     id: int
     caller_number: str | None = None
     caller_name: str | None = None
+    complaint_reason: str | None = None
+    notes: str | None = None
+    stay_date: str | None = None
+    call_time: str | None = None
     
+                        
